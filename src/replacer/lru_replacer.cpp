8ca81ed4ee5035d31e6dafb4ba330c6ef0425649
yjmstr <pyjmstr@gmail.com>
2022-12-16 15:26:40 +0800

    1.1.2 缓冲池替换策略

--- src/replacer/lru_replacer.cpp
+++ src/replacer/lru_replacer.cpp
@@ -5,7 +5,7 @@ LRUReplacer::LRUReplacer(size_t num_pages) { max_size_ = num_pages; }
 LRUReplacer::~LRUReplacer() = default;
 
 /**
- * @brief 使用LRU策略删除一个victim frame，这个函数能得到frame_id
+ * @brief 使用 LRU 策略删除一个 victim frame，这个函数能得到 frame_id
  * @param[out] frame_id id of frame that was removed, nullptr if no victim was found
  * @return true if a victim frame was found, false otherwise
  */
@@ -15,36 +15,50 @@ bool LRUReplacer::Victim(frame_id_t *frame_id) {
     std::scoped_lock lock{latch_};
 
     // Todo:
-    //  利用lru_replacer中的LRUlist_,LRUHash_实现LRU策略
-    //  选择合适的frame指定为淘汰页面,赋值给*frame_id
-
+    //  利用 lru_replacer 中的 LRUlist_,LRUHash_实现 LRU 策略
+    //  选择合适的 frame 指定为淘汰页面，赋值给*frame_id
+    if (LRUlist_.empty()) {
+        frame_id = NULL;
+        return false;
+    }
+    *frame_id = *LRUlist_.rbegin();
+    LRUlist_.pop_back();
+    LRUhash_.erase(*frame_id);
     return true;
 }
 
 /**
- * @brief 固定一个frame, 表明它不应该成为victim（即在replacer中移除该frame_id）
+ * @brief 固定一个 frame, 表明它不应该成为 victim（即在 replacer 中移除该 frame_id）
  * @param frame_id the id of the frame to pin
  */
 void LRUReplacer::Pin(frame_id_t frame_id) {
     std::scoped_lock lock{latch_};
     // Todo:
-    // 固定指定id的frame
-    // 在数据结构中移除该frame
+    // 固定指定 id 的 frame
+    // 在数据结构中移除该 frame
+    if (LRUhash_.count(frame_id)) {
+        LRUlist_.erase(LRUhash_[frame_id]);
+        LRUhash_.erase(frame_id);
+    }
 }
 
 /**
- * 取消固定一个frame, 表明它可以成为victim（即将该frame_id添加到replacer）
+ * 取消固定一个 frame, 表明它可以成为 victim（即将该 frame_id 添加到 replacer）
  * @param frame_id the id of the frame to unpin
  */
 void LRUReplacer::Unpin(frame_id_t frame_id) {
     // Todo:
     //  支持并发锁
-    //  选择一个frame取消固定
+    //  选择一个 frame 取消固定
+    std::scoped_lock lock{latch_};
+    LRUlist_.push_front(frame_id);
+    std::list<frame_id_t>::iterator it = LRUlist_.begin();
+    LRUhash_.insert(frame_id, *it);
 }
 
-/** @return replacer中能够victim的数量 */
+/** @return replacer 中能够 victim 的数量 */
 size_t LRUReplacer::Size() {
     // Todo:
-    // 改写return size
-    return 0;
+    // 改写 return size
+    return LRUlist_.size();
 }
